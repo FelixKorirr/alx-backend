73f5be227ae7e73aad6f40c82bad6d29de7113c1
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-04 23:06:06 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,8 +5,8 @@
 def index_range(page, page_size):
     '''returns a tuple of size two containing
     a start index and end index'''
-    data = list(range(0, 101))
-    start_idx = ((page - 1) * 10)
+    data = list(range(0, 200))
+    start_idx = (page - 1) * page_size
     end_idx = start_idx + page_size
 
     return (data[start_idx], data[end_idx])
