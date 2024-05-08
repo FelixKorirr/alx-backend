c9d6654ef29b6f8ce2ca23ffd432d4a790693d42
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-08 12:01:44 +0300

    caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Task 0 module'''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''Represents subclass BasicCache'''
+
+    def put(self, key, item):
+        '''Adds key value pairs to a dictionary
+            Args:
+                key : unique identifier
+                item : value to be added
+        '''
+        if key == None or item == None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''Accesses the value using the unique ID
+            Args:
+                key : unique ID
+        '''
+        if key == None:
+            return None
+        else:
+            for k, v in self.cache_data.items():
+                if k == key:
+                    return v
