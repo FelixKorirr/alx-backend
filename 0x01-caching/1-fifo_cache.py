23246a0e77d3e5214e565cabc6a55bebd165fffa
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-08 13:07:44 +0300

    caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''This module demonstrates FIFO caching'''
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    '''This class represent the subclass FIFOCache'''
+    def __init__(self):
+        '''Initializes this subclass'''
+        super().__init__()
+        self.cache_keys = []
+
+    def put(self, key, item):
+        '''Adds key-value pairs to cache '''
+        if key is None or item is None:
+            pass
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest_key = self.cache_keys.pop(0)
+            print(f'DISCARD : {oldest_key}')
+            del self.cache_data[oldest_key]
+        else:
+            self.cache_data[key] = item
+            self.cache_keys.append(key)
+
+    def get(self, key):
+        '''Returns the values in self.cache_data'''
+        if key is None:
+            return None
+        else:
+            for k, v in self.cache_data.items():
+                if k == key:
+                    return v
